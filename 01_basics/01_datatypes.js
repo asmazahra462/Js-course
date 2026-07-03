aea03609a0c613b3eca4990988bdd5012097e581
asmazahra462 <asmazahra462@gmail.com>
2026-07-03 16:54:10 +0000

    variables and datatypes done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// number 
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "asma");
---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 12211
+let accountEmail = "abc@gmail.com"
+var accountPassword = "123"
+accountCity = "Mardan"
+
+// accountId = 2 not allowed
+
+accountEmail = "ab@ab.com"
+accountPassword = "2222"
+accountCity = "Peshawer"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
