fe7139858cbbc58d5e766765cbf80553aa20effb
asmazahra462 <asmazahra462@gmail.com>
2026-07-04 16:54:12 +0000

    operations done

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -19,3 +19,22 @@ console.log(typeof stringNumber);
 
 
 
+// *************** Operations *******************
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Asma"
+let str3 = str1 + str2 
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
